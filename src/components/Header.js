d703ad37c201afcc6bb0f9a2cf4fc1c17e53db95
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-03-16 12:09:57 -0300

    First version of App + base Header

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="images/Logo.png" alt="ZapRecall Logo"/>
+            <h1>ZapRecall</h1>
+        </header>
+    )
+}
