ce458a5a0b7c9109c50451183004f97ccc4d56da
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-03-19 18:57:54 -0300

    User can choose how many cards to show on the screen

--- src/components/FirstScreen/FirstScreen.js
+++ src/components/FirstScreen/FirstScreen.js
@@ -8,20 +8,25 @@ export default function FirstScreen() {
     const [show, setShow] = useState(false)
     const [valid, setValid] = useState(true)
     const [input, setInput] = useState('')
+    const [cardNumber, setCardNumber] = useState('')
     const [finalDeck, setFinalDeck] = useState([])
     let decksName = Object.keys(Decks)
     let decksArray = Object.values(Decks)
 
     function Screen() { 
+        let validateInput = input > finalDeck.length || input <= 0 || !parseInt(input) || parseInt(input) > parseInt(cardNumber)
+        let validateCardNumber = cardNumber > finalDeck.length || cardNumber <= 0 || !parseInt(cardNumber)
+
         function changeDeck(index) {
             let deckIndex = index.target.value
             setFinalDeck(decksArray[deckIndex])
         }
         
         function validate() {
-            if (parseInt(input) > finalDeck.length || parseInt(input) <= 0 || !parseInt(input)) {
+            if (validateInput || validateCardNumber) {
                 setValid(false)
             } else {
+                finalDeck.sort(() => Math.random() - 0.5)
                 setValid(true)
                 setShow(true)
             }
@@ -29,33 +34,37 @@ export default function FirstScreen() {
 
         return (
             <div className="deck-selection">
-                <div>
+                <div className='logo'>
                     <img src={Logo} alt="ZapRecall logo"></img>
                     <h1>ZapRecall</h1>
                 </div>
-                <Select changeDeck={changeDeck} decksName={decksName} decksArray={decksArray} />
+                <Select changeDeck={changeDeck} decksName={decksName} decksArray={decksArray} 
+                cardNumber={cardNumber} setCardNumber={setCardNumber}/>
                 <input placeholder='Input your Zap goal...' value={input} onInput={e => setInput(e.target.value)}></input>
                 <button onClick={validate}>Start Recall!!</button>
-                {!valid ? <p>Enter a valid goal</p> : <></>}
+                {!valid ? <p>Enter a valid card number/goal</p> : <></>}
             </div>
         )
     }
 
     return (
-        show ? <Main input={input} deck={finalDeck.sort(() => Math.random() - 0.5)} /> : Screen()
+        show ? <Main input={input} deck={finalDeck.slice(0, cardNumber)} /> : Screen()
     )
 }
 
-function Select({changeDeck, decksName, decksArray}) {
+function Select({changeDeck, decksName, decksArray, cardNumber, setCardNumber}) {
     return (
-        <select onChange={option => changeDeck(option)}>
-            <option value="" hidden>Choose your deck...</option>
-            {decksName.map(
-                (deck, i) => 
-                <option key={deck} value={i}>
-                    {deck[0].toUpperCase() + deck.slice(1)}, {decksArray[i].length} cards
-                </option>
-            )}
-        </select>
+        <div className='choose-deck'>
+            <select onChange={option => changeDeck(option)}>
+                <option value="" hidden>Choose your deck...</option>
+                {decksName.map(
+                    (deck, i) => 
+                    <option key={deck} value={i}>
+                        {deck[0].toUpperCase() + deck.slice(1)}, {decksArray[i].length} cards
+                    </option>
+                )}
+            </select>
+            <input placeholder='Number of cards...' value={cardNumber} onInput={e => setCardNumber(e.target.value)}></input>
+        </div>
     )
 }
